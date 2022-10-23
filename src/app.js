56afa096baacf8ffad9c636708c10de6e84c06a0
Elcio Yoshiaki Sato <elciosato@gmail.com>
2022-10-23 10:30:01 +0200

    add session configuration

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const session = require("express-session")
 
 const app = express();
 const router = require("./routes")
@@ -14,6 +15,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname,"../public")))
+
+const sessionConfig = {
+    secret: "4024fc2e5d63bb2cc1c8850b7ee984c4",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig))
 
 mongoose.connect('mongodb://yelpcampUsr:y3lpcampUsr123@localhost/yelpcampDB');
 
